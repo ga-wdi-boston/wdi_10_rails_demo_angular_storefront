f72e73df7b964386bc5ebf61608e37eb7b8e93d0
David Fisher <tibbon@gmail.com>
2013-12-03 13:02:19 -0500

    Fixed the Lambda scope

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
 
-  scope :in_cart, -> {'order_id' == nil}
+  scope :in_cart, -> {where order_id: nil}
+  scope :checked_out, -> {where "order_id IS NOT NULL" }
 
 end
