a63d793232279d3b59ee025e41b72a88f0f9af76
Tom Dyer <tdyer1@gmail.com>
2013-12-03 16:45:58 -0500

    Add route parameters.

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,12 +1,12 @@
 var IndexCtrl = function($scope, $location){                                              
 
  $scope.data = {products: [
-  {name: 'Product 1', description: "Product 1 description", price: 40.23},
-  {name: 'Product 2', description: "Product 2 description", price: 4.35},
-  {name: 'Product 3', description: "Product 3 description", price: 20.45}
+  {name: 'Product 0', description: "Product 1 description", price: 40.23},
+  {name: 'Product 1', description: "Product 2 description", price: 4.35},
+  {name: 'Product 2', description: "Product 3 description", price: 20.45}
   ]};
 
-  $scope.viewPost = function(){
-    $location.url('/product');
+  $scope.viewPost = function(productId){
+    $location.url('/product/' + productId);
   }
 };          
--- app/assets/javascripts/controllers/main/mainProductCtrl.js
+++ app/assets/javascripts/controllers/main/mainProductCtrl.js
@@ -1,6 +1,8 @@
-var ProductCtrl = function($scope){                                              
+var ProductCtrl = function($scope, $routeParams){                                              
 
 	$scope.data = {product:
 		{name: 'Product 1', description: "Product 1 description", price: 40.23}
 	};
+
+	$scope.data.productId = $routeParams.productId;
 }; 
