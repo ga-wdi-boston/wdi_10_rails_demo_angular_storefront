aab7e57fe34abfae1d8f1fa995a0f741aeff8324
Tom Dyer <tdyer1@gmail.com>
2013-12-03 14:20:07 -0500

    Add products angular view.

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,3 +1,8 @@
 var IndexCtrl = function($scope){                                              
-  $scope.title = "Products";                                                    
-}          
+
+ $scope.data = {products: [
+  {name: 'Product 1', description: "Product 1 description", price: 40.23},
+  {name: 'Product 2', description: "Product 2 description", price: 4.35},
+  {name: 'Product 3', description: "Product 3 description", price: 20.45}
+  ]};
+};          
