335a7d14e8bfeff38a8038e8bf6976d394b1966c
Tom Dyer <tdyer1@gmail.com>
2013-12-03 16:27:22 -0500

    Add product angular view.

---
+++ app/assets/javascripts/controllers/main/mainProductCtrl.js
@@ -0,0 +1,6 @@
+var ProductCtrl = function($scope){                                              
+
+	$scope.data = {product:
+		{name: 'Product 1', description: "Product 1 description", price: 40.23}
+	};
+}; 
