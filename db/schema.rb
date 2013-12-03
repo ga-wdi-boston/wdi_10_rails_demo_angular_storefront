db61b5cbe2bf4987bb95874564ce3846bd382fa3
Tom Dyer <tdyer1@gmail.com>
2013-12-03 13:27:33 -0500

    Add angular rails gem

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131203164232) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "line_items", force: true do |t|
     t.integer  "user_id"
     t.integer  "product_id"
