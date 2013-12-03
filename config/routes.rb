abb963ef24a4cf6506005be065077f77826ed174
David Fisher <tibbon@gmail.com>
2013-12-03 12:19:05 -0500

    Added stripe config initializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Storefront::Application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :show]
+  resources :orders, only: [:new, :create]
   post '/products/:product_id/add_to_cart', to: 'line_items#create', as: 'add_product_to_cart'
   get '/cart', to: 'line_items#index', as: 'cart'
   delete '/cart/:id', to: 'line_items#delete', as: 'remove_product_from_cart'
