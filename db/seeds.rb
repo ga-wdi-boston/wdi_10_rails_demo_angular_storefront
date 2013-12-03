23767dd2c31dfe2028fdbc55a7cd8178701d84a2
David Fisher <tibbon@gmail.com>
2013-12-03 10:18:45 -0500

    Updated Product migration. Creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.new(name: "Seiki Digital SE39UY04 39-Inch 4K Ultra HD 120Hz LED TV",
+						description: "Remember how excited you were the first time you saw HD? Multiply that by four and you'll understand 4K Ultra HD from Seiki. With over 8 million individual pixels (3840 x 2160) compared to about 2 million (1920 x 1080) on your current HDTV, the picture provides stunning clarity and colors that far surpass anything you've seen before. Put simply, you have to see it to believe it.",
+						price: 399.99)
+
+Product.new(name: "The Elder Scrolls Anthology",
+						description: "Arena: The Anthology features the original version of Arena and will launch within a DOS emulator.
+Daggerfall: The ancient golem Numidium, a powerful weapon once used to unify Tamriel has once again been unearthed. The Anthology features the original version of Daggerfall and will launch within a DOS emulator.
+Morrowind: To fulfill an ancient prophecy, the Emperor sends you, to the island of Vvardenfell to rid the land of Morrowind of a dark curse. Also includes: Tribunal and Bloodmoon add-ons, plus The Elder Scrolls Construction Set.
+Oblivion: With the Empire ready to crumble, the gates of Oblivion open and demons march upon the land. In the shadow of evil, a hero will rise from the ashes of a fallen Empire. Also includes: Knights of the Nine and Shivering Isles expansions.
+Skyrim: Dragons, long lost to the passages of the Elder Scrolls, have returned to Tamriel. Also includes: Dawnguard, Hearthfire, and Dragonborn add-ons.",
+						price: 59.99)
