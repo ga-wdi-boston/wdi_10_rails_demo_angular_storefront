41fcab774c2b9a22ca87b332f7219aeb108549c6
David Fisher <tibbon@gmail.com>
2013-12-03 11:54:43 -0500

    Some logic for building line items in place

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,11 @@
 class LineItemsController < ApplicationController
 	def create
+		@product = Product.find(params[:product_id])
+		@line_item = @product.line_items.build(user: current_user)
 		binding.pry
+	end
+
+	def index
+		@line_items = LineItems.all
 	end
 end
